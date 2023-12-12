0a0255fdfd81a579580a8b51a4cd60c1c94fda7a
AdRosing22 <adamrosinghd@gmail.com>
2023-12-12 16:13:25 +0100

    Skapar main method i main klass

---
+++ src/MenInBlack/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MenInBlack;
+
+/**
+ *
+ * @author adamrosing
+ */
+public class Main {
+    
+    public static void main(String[] args)
+    {
+        Huvudfonster hf = new Huvudfonster();
+        
+        hf.setVisible(true);
+            
+    }
+    
+}
