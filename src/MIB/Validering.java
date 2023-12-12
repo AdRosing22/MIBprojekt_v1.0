792a6846c3f423049255311949b390ec7bec1ecd
AdRosing22 <77940488+AdRosing22@users.noreply.github.com>
2023-12-12 22:45:39 +0100

    Skapat main och huvudfönster
    Vette fan vad jag gjort men försökt skapa GUI för huvudfönster där man ska logga in. Även lagt grund för main metodet i Start klassen. Utöver det skapade jag valideringsklassen vi ska ha.

---
+++ src/MIB/Validering.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MIB;
+
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
+/**
+ *
+ * @author Adam
+ */
+public class Validering {
+    
+    private static InfDB idb;
+    
+    public Validering(InfDB idb)
+    {
+        this.idb = idb;
+    }
+    
+    
+    public static boolean finnsAnvandareEpostIDB(String epost)
+    {
+        boolean finns = false;
+        String dbEpost;
+        
+        String fragaAgent = "SELECT epost FROM agent WHERE epost='"+epost+"'";
+        String fragaAlien = "SELECT epost FROM alien WHERE epost='"+epost+"'";
+        
+        //kör fråga mot databas
+        
+        try{
+            dbEpost = idb.fetchSingle(fragaAgent);
+            
+            if(dbEpost.equals(epost))
+            {
+                finns = true;
+            }
+            else
+            {
+                dbEpost = idb.fetchSingle(fragaAlien);
+                if(dbEpost.equals(dbEpost));
+                {
+                    finns = true;
+                }
+            }
+        }
+        catch (InfException e)
+        {
+            System.out.println("Ingen epost hittades"+e);
+        }
+        
+        return finns;
+    }
+    
+    
+    
+}
