29f0ae826de17f49bf7d4e593f5cec45ba4e644c
AdRosing22 <adamrosinghd@gmail.com>
2023-12-12 17:03:06 +0100

    uppdaterad paket
    delar på klasser och fönster i projektets source paket

--- src/klasser/Main.java
+++ src/klasser/Main.java
@@ -2,20 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package MenInBlack;
+package klasser;
 
 /**
  *
  * @author adamrosing
  */
 public class Main {
-    
-    public static void main(String[] args)
-    {
-        Huvudfonster hf = new Huvudfonster();
-        
-        hf.setVisible(true);
-            
-    }
     
 }
