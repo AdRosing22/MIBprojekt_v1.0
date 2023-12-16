530d586ed29d701243c749abda42d44e495b4abc
AdRosing22 <77940488+AdRosing22@users.noreply.github.com>
2023-12-16 15:25:06 +0100

    Update Main.java

--- src/MIB/Main.java
+++ src/MIB/Main.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package MIB;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JOptionPane;
+
 import oru.inf.InfDB;
 import oru.inf.InfException;
 
