d21d5e57886894db39aae3855063943dea539ee1
melkerlangell <75675951+melkerlangell@users.noreply.github.com>
2023-12-27 15:34:53 +0100

    knapp

--- src/MIB/HanteraAliensFonster.java
+++ src/MIB/HanteraAliensFonster.java
@@ -39,6 +39,7 @@ public class HanteraAliensFonster extends javax.swing.JFrame {
         btnRegNyAlien = new javax.swing.JToggleButton();
         btnRedigeraAlien = new javax.swing.JToggleButton();
         btnBacka = new javax.swing.JToggleButton();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -66,16 +67,12 @@ public class HanteraAliensFonster extends javax.swing.JFrame {
             }
         });
 
+        jButton1.setText("Ta bort alien");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(btnRedigeraAlien, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(btnRegNyAlien, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()
                 .addGap(131, 131, 131)
                 .addComponent(jTtitel, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -83,6 +80,13 @@ public class HanteraAliensFonster extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(16, 16, 16)
                 .addComponent(btnBacka, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(btnRedigeraAlien, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnRegNyAlien, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -92,9 +96,11 @@ public class HanteraAliensFonster extends javax.swing.JFrame {
                 .addComponent(jTtitel)
                 .addGap(59, 59, 59)
                 .addComponent(btnRegNyAlien)
-                .addGap(43, 43, 43)
+                .addGap(32, 32, 32)
                 .addComponent(btnRedigeraAlien)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 86, Short.MAX_VALUE)
+                .addGap(30, 30, 30)
+                .addComponent(jButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 40, Short.MAX_VALUE)
                 .addComponent(btnBacka)
                 .addGap(20, 20, 20))
         );
@@ -135,6 +141,7 @@ public class HanteraAliensFonster extends javax.swing.JFrame {
     private javax.swing.JToggleButton btnBacka;
     private javax.swing.JToggleButton btnRedigeraAlien;
     private javax.swing.JToggleButton btnRegNyAlien;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jTtitel;
     // End of variables declaration//GEN-END:variables
 }
